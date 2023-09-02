32a52be5026d57941a990487e678dbe18061f9f9
donghoonyeom <dua5410773@naver.com>
2023-09-02 19:59:25 +0900

    회원가입시 비밀번호 암호화 추가 2

--- server/src/main/java/com/mainproject/server/user/repository/UserRepository.java
+++ server/src/main/java/com/mainproject/server/user/repository/UserRepository.java
@@ -1,19 +1,9 @@
 package com.mainproject.server.user.repository;
 
 import com.mainproject.server.user.entity.User;
-import org.hibernate.SessionFactory;
-import org.hibernate.dialect.function.StandardSQLFunction;
-import org.hibernate.internal.SessionFactoryImpl;
-import org.hibernate.type.IntegerType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.websocket.Session;
-import java.util.List;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
