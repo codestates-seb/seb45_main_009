e9452deabbe2ebe6b247800a4b60ab37e31aa01f
lse0522 <88942643+lse0522@users.noreply.github.com>
2023-09-05 14:54:35 +0900

    틀, 값 저장

--- client/src/pages/Loginaddition.tsx
+++ client/src/pages/Loginaddition.tsx
@@ -1,9 +1,15 @@
-import CommonInput from '../components/atoms/CommonInput'
+// import CommonInput from '../components/atoms/CommonInput'
 import BlueButton from '../components/atoms/BlueButton' 
 import React, { useState } from 'react';
+import UserInfoForm from  '../components/atoms/UserIndfrom';
+import UserCorForm from  '../components/atoms/UserCorfrom';
+
+
 
 
 function Loginaddition() {
+    const [ind, setind] = useState<boolean>(false);
+
     // 미리보기 url 저장
     const [previewImage, setPreviewImage] = useState<string | null>(null);
 
@@ -14,53 +20,44 @@ function Loginaddition() {
             // 이미지 미리보기 설정
             const imageUrl = URL.createObjectURL(selectedImage);
             setPreviewImage(imageUrl);
+            setUserInfo(prev => ({ ...prev, imageUrl: imageUrl }));
         }
     };
 
     const  [userInfo, setUserInfo] =  useState({
+        imageUrl : '',
         height : 0,
         weight : 0,
         location : '',
         exercise  : '',
-        selfintroduction : ''
+        selfintroduction : '',
+        phonenumber : '',
+        price : ''
     })
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, key: keyof typeof userInfo) => {
+    const handleInputChange :any = (e: React.ChangeEvent<HTMLInputElement>, key: keyof typeof userInfo) => {
         setUserInfo(prev => ({ ...prev, [key]: e.target.value }));
     };
     
     const handleRegister = () => {
-        // console.log("저장한값"+userInfo); // 사용자 정보 객체를 로깅
-        console.log('버튼 클릭')
-        // 추가적인 처리
+        console.log(userInfo)
     };
+
 
   return (
     < div className="flex  justify-center">
         <div className="w-[400px]">
             <input type='file'  onChange={handleImageUpload} />
             <div className='flex justify-center'>
-                {previewImage && <img src={previewImage} alt="Preview" className="mt-4 rounded-full w-[200px] h-[200px]" />}
+                {previewImage && <img src={previewImage} alt="Preview" className="mt-4 rounded-full w-[100px] h-[100px] border" />}
             </div>
-
-            <div className='flex'>
-                <div className='mr-[80px]'>
-                    <CommonInput placeholder="키" type="text" onChange={(e) => handleInputChange(e, 'height')} />
-                </div>
-                <CommonInput placeholder="몸무게" type="text" onChange={(e) => handleInputChange(e, 'weight')} />
+            { ind === true ? 
+                <UserInfoForm handleInputChange={handleInputChange}/> :
+                <UserCorForm handleInputChange={handleInputChange}/>  }
+            
+            <div className='flex justify-center mt-[30px]'>
+                <button className='w-full h-[30px] rounded-[4px] text-[14px] mt-[20px] font-medium bg-btn-color text-white' onClick={handleRegister}>등록</button>
             </div>
-            <div className='flex'>
-                <div className='mr-[80px]'>
-                    <CommonInput placeholder="지역" type="text" onChange={(e) => handleInputChange(e, 'location')} />
-                </div>
-                <CommonInput placeholder="주 운동 종목" type="text" onChange={(e) => handleInputChange(e, 'exercise')} />
-            </div>
-            <CommonInput placeholder="자기소개" type="text" onChange={(e) => handleInputChange(e, 'selfintroduction')} />
-
-            <div className='flex justify-center'>
-                <BlueButton label='등록' className='w-full' onClick={handleRegister}/>
-            </div>
-            {/* <div>건너뛰기</div> */}
         </div>
     </ div>
   );
