b22b9a14612ab3290acde8f1fd0b9669b2c0c668
Beomda <ssb1pro@gmail.com>
2023-09-06 22:40:27 +0900

    feat: CICD

--- server/src/main/java/com/mainproject/server/user/entity/User.java
+++ server/src/main/java/com/mainproject/server/user/entity/User.java
@@ -2,10 +2,7 @@ package com.mainproject.server.user.entity;
 
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 import javax.persistence.*;
