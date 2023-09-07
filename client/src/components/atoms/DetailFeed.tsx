1027fb296c097f7e46e0b23ab584ed25d6b1aa03
lse0522 <88942643+lse0522@users.noreply.github.com>
2023-09-08 02:32:19 +0900

    정보태그, 리액트 아이콘 추가

--- client/src/components/atoms/DetailFeed.tsx
+++ client/src/components/atoms/DetailFeed.tsx
@@ -1,11 +1,13 @@
 import { useState, useRef, useEffect } from "react";
 import {AiOutlineHeart , AiFillHeart} from 'react-icons/ai';
 import {RiAlarmWarningFill} from 'react-icons/ri';
+import {AiFillPlusCircle} from 'react-icons/ai';
+
 
 
 function TagModal({ title, size, price }: { title: string; size: string; price: number }) {
   return (
-    <div className="border w-[100px] rounded-[2px] bg-white text-[12px] mt-[30px] pl-[10px]">
+    <div className="border w-[100px] rounded-[2px] bg-white text-[12px] mt-[2px] pl-[10px]">
       <div className="">{title}</div>
       <div className="text-gray-400 text-[8px]">{size}</div>
       <div className="font-bold">₩{price}</div>
@@ -26,17 +28,21 @@ function DetailFeedCor() {
     content : '오운완!!',
     tag : ['크로스핏', '헬스']
   }
-  let tagData : {  
-    taglocation : string[],
-    title : string,
-    size : string,
-    price : number
-  } = {
-    taglocation : ["50px", "50px"],
-    title : 'adidas',
-    size : 'XL',
-    price : 33000
-  }
+
+  // 정보 태그 데이터
+  let tagDatas = [{
+    taglocation: ["50px 50px", "400px 100px", "200px 400px"],
+    title: ['adidas', 'Nike', 'adidas'],
+    size: ['XL', '280', "s"],
+    price: [33000, 99000, 20000]
+  },
+  {
+    taglocation: ["100px 50px", "200px 200px", "100px 400px"],
+    title: ['2adidas', '2Nike', '2adidas'],
+    size: ['XL', '280', "s"],
+    price: [233000, 299000, 220000]
+  }];
+
 
   // 좋아요
   const [isLiked, setIsLiked] = useState(false);
@@ -55,24 +61,39 @@ function DetailFeedCor() {
   }
 
   // 정보 태그 모달창
-  const [showTagModal, setShowTagModal] = useState(false);
-
+  // 모달창이 photoindex와 tagindex 일치 불일치
+  const [showTagModal, setShowTagModal] = useState<{ photoIndex: number; tagIndex: number } | null>(null);
 
 
   return(
     // 화면 최대 넓이를 중간크기로, 수평 중앙 위치, 패딩조절
     <div className='w-full sm:max-w-screen-sm  mx-auto px-4 sm:px-4 lg:px-8'>
-        {feedData.photo.map((photo, index) => (
-        <div key={index} className="mb-8 relative">
+      {feedData.photo.map((photo, photoIndex) => (
+        <div key={photoIndex} className="mb-8 relative">
           <img src={photo} className="w-full h-auto" />
-          <div
-            className="w-[20px] h-[20px] bg-red-500 rounded-full absolute"
-            style={{ top: tagData.taglocation[0], left: tagData.taglocation[1] }}
-            onMouseEnter={() => setShowTagModal(true)}
-            onMouseLeave={() => setShowTagModal(false)}
-          >
-            {showTagModal && <TagModal title={tagData.title} size={tagData.size} price={tagData.price} />}
-            </div>
+          {tagDatas[photoIndex].taglocation.map((location, tagIndex) => {
+            // 띄어쓰기로 top,left 나누기
+            const [top, left] = location.split(' ');
+            return (
+              <div
+                key={tagIndex}
+                className="w-[20px] h-[20px] rounded-full absolute"
+                style={{ top, left }}
+                onMouseEnter={() => setShowTagModal({ photoIndex, tagIndex })}
+                onMouseLeave={() => setShowTagModal(null)}
+              >
+                <AiFillPlusCircle className="w-[20px] h-[20px] text-red-500" />
+                {/* 만약 div위에 마우스 올린게 이미지index와 태그 index가 맞으면 모달창 보여주기 */}
+                {showTagModal?.photoIndex === photoIndex && showTagModal?.tagIndex === tagIndex &&
+                  <TagModal
+                    title={tagDatas[photoIndex].title[tagIndex]}
+                    size={tagDatas[photoIndex].size[tagIndex]}
+                    price={tagDatas[photoIndex].price[tagIndex]}
+                  />
+                }
+              </div>
+            )
+          })}
         </div>
       ))}
         <div className="font-bold text-gray-400 text-sm mt-[10px]" >{feedData.date}</div>
