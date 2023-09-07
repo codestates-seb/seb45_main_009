9f9d1335cdfc28cec64910185b7cfe500bf52511
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2023-09-07 14:06:14 +0900

    Update BusinessLogicException.java

--- server/src/main/java/com/mainproject/server/exception/BusinessLogicException.java
+++ server/src/main/java/com/mainproject/server/exception/BusinessLogicException.java
@@ -1,9 +1,8 @@
 package com.mainproject.server.exception;
 
-
 import lombok.Getter;
 
-public class BusinessLogicException extends RuntimeException {
+public class BusinessLogicException extends RuntimeException{
     @Getter
     private ExceptionCode exceptionCode;
 
