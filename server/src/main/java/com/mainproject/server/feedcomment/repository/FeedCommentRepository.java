a8592b0540352adf75f0267c27b4bfcf49b9df13
Beomda <ssb1pro@gmail.com>
2023-09-07 14:27:35 +0900

    fix: exceptioncode 수정

--- server/src/main/java/com/mainproject/server/feedcomment/repository/FeedCommentRepository.java
+++ server/src/main/java/com/mainproject/server/feedcomment/repository/FeedCommentRepository.java
@@ -1,6 +1,9 @@
 package com.mainproject.server.feedcomment.repository;
 
+import com.mainproject.server.feed.enitiy.Feed;
 import com.mainproject.server.feedcomment.entity.FeedComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
