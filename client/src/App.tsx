0eb14716f17f9b1f99dd82c15f61ec99db6726b5
TaeYoungPar <tpark@oswego.edu>
2023-09-08 11:40:39 +0900

    feat 스크롤위치에 따른 up버튼 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,15 @@
-import React, { PropsWithChildren, useEffect } from "react";
-import "./App.css";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import axios from "axios";
-
 import { login } from "./redux/reducers/loginSlice";
-
 import { UserInfo } from "./types/types";
-
 import Header from "./components/sharedlayout/Header";
 import Footer from "./components/sharedlayout/Footer";
-
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Profile from "./pages/Profile";
 import Loginaddition from "./pages/Loginaddition";
-
 import MainPageInd from "./pages/MainPageInd";
-// import MainPageCor from "./pages/MainPageCor";
 import FeedDetailPageCor from "./pages/FeedDetailPageCor";
 import FeedDetailPageInd from "./pages/FeedDetailPageInd";
 import FeedFormPageCor from "./pages/FeedFormPageCor";
@@ -25,27 +18,20 @@ import Not404 from "./pages/Not404";
 import MainPageCor from "./pages/MainPageCor";
 import MyPage from "./pages/MyPage";
 import ChangePassword from "./components/atoms/ChangePassword";
-
 import { useDispatch } from "react-redux";
 import Top from "./components/atoms/Top";
 import Up from "./components/atoms/Up";
 import MyPageTop from "./components/atoms/MyPageTop";
 import ScrollToTop from "./components/features/ScrollToTop";
-
-const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  const location = useLocation();
-  const isMyPage = location.pathname.includes("/mypage");
-  return (
-    <>
-      {isMyPage ? <MyPageTop /> : <Top />}
-      {children}
-      <Up />
-    </>
-  );
-};
+import Layout from "./components/atoms/Layout";
 
 function App() {
   const dispatch = useDispatch();
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
 
   useEffect(() => {
     const accessToken = sessionStorage.getItem("access_token");
@@ -62,7 +48,15 @@ function App() {
         console.error("Error decoding user info:", error);
       }
     }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, [dispatch]);
+
+  const showUpButton = scrollY > 100;
 
   return (
     <BrowserRouter>
@@ -74,20 +68,14 @@ function App() {
             <Routes>
               <Route path="/" element={<MainPageInd />}></Route>
               <Route path="/store" element={<MainPageCor />}></Route>
-              <Route
-                path="/feeddetailcor"
-                element={<FeedDetailPageCor />}
-              ></Route>
-              <Route
-                path="/feeddetailind"
-                element={<FeedDetailPageInd />}
-              ></Route>
-              <Route path="/feedformcor" element={<FeedFormPageCor />}></Route>
-              <Route path="/feedformind" element={<FeedFormPageInd />}></Route>
+              <Route path="/feeddetailcor" element={<FeedDetailPageCor />} />
+              <Route path="/feeddetailind" element={<FeedDetailPageInd />} />
+              <Route path="/feedformcor" element={<FeedFormPageCor />} />
+              <Route path="/feedformind" element={<FeedFormPageInd />} />
               <Route path="/mypage/:page" element={<MyPage />} />
               <Route path="/login" element={<LoginPage />}></Route>
               <Route path="/signup" element={<SignupPage />}></Route>
-              <Route path="/loginaddition" element={<Loginaddition />}></Route>
+              <Route path="/loginaddition" element={<Loginaddition />} />
               <Route path="/profile" element={<Profile />}></Route>
               <Route path="*" element={<Not404 />} />
             </Routes>
--- client/src/components/atoms/Up.tsx
+++ client/src/components/atoms/Up.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { FiArrowUpCircle } from "react-icons/fi";
+
 const Up = () => {
   const scrollUp = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  // window.scrollY 값이 200 이상일 때 버튼을 표시
+  const showUpButton = window.scrollY > 400;
+
   return (
-    <div className="fixed bottom-10 right-0 mb-10 mr-10 hover:cursor-pointer z-50">
-      <FiArrowUpCircle
-        size={30}
-        onClick={scrollUp}
-        className=" sm:w-[7vw] sm:h-[7vh]"
-      />
-    </div>
+    // showUpButton 값에 따라 버튼을 조건부로 렌더링
+    showUpButton ? (
+      <div className="fixed bottom-10 right-0 mb-10 mr-10 hover:cursor-pointer z-50">
+        <FiArrowUpCircle
+          size={30}
+          onClick={scrollUp}
+          className="sm:w-[7vw] sm:h-[7vh]"
+        />
+      </div>
+    ) : null
   );
 };
 
