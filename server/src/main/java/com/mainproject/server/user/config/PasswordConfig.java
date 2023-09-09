922eb39ea319d12ce4849727f97c4d7403ece463
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2023-09-09 17:45:06 +0900

    Update PasswordConfig.java

--- server/src/main/java/com/mainproject/server/user/config/PasswordConfig.java
+++ server/src/main/java/com/mainproject/server/user/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.mainproject.server.user.config;//package com.mainproject.server.user.config;
+//package com.mainproject.server.user.config;
 //
 //
 //import org.springframework.context.annotation.Bean;
