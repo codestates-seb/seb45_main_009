6c3f1d91495dcc67e03a37fe479cc04ec04855c5
Beomda <109846158+Beomda@users.noreply.github.com>
2023-09-09 15:34:16 +0900

    Update ServerApplicationTests.java
    테스트 코드 주석

--- server/src/test/java/com/mainproject/server/ServerApplicationTests.java
+++ server/src/test/java/com/mainproject/server/ServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mainproject.server;
+// package com.mainproject.server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ServerApplicationTests {
+// @SpringBootTest
+// class ServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
