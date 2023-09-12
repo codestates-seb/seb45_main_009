596bb3cb08af28ff4488df1689adccb0c6a89821
hamjaehyeong <ham6729@gmail.com>
2023-09-12 19:52:38 +0900

    feat feedform ballontag (work in progress)

--- client/src/pages/FeedFormPageInd.tsx
+++ client/src/pages/FeedFormPageInd.tsx
@@ -111,6 +111,33 @@ function FeedFormPageInd() {
         },
       });
       console.log(response);
+      const imageIds = response.data.images.map((imageData: any) => imageData.imageId);
+
+      for (let i = 0; i < imageIds.length; i++) {
+        const imageId = imageIds[i];
+        const imgTagData = previewImg[i].tags;
+
+        for (const tagData of imgTagData) {
+          const tagPostData = {
+            productName: tagData.data?.name,
+            productPrice: tagData.data?.price,
+            productInfo: tagData.data?.info,
+            positionX: tagData.x,
+            positionY: tagData.y,
+          };
+
+          const requestData = {
+            imageTag: JSON.stringify(tagPostData),
+          };
+
+          try {
+            await axios.post(`http://13.125.146.181:8080/image/${imageId}`, requestData);
+          } catch (error) {
+            console.error("error", error);
+          }
+        }
+      }
+      // response.data.images
       // alert("포스팅 성공");
       // navigate("/");
     } catch (error: any) {
