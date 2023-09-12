807b207767acaf2308216bdd184f585f31127528
donghoonyeom <dua5410773@naver.com>
2023-09-13 00:12:25 +0900

    feat 유저 닉네임,연관 태그 검색 추가

---
+++ server/src/main/java/com/mainproject/server/search/service/SearchService.java
@@ -0,0 +1,73 @@
+package com.mainproject.server.search.service;
+
+import com.mainproject.server.feed.dto.FeedDto;
+import com.mainproject.server.feed.enitiy.Feed;
+import com.mainproject.server.feed.repository.FeedRepository;
+import com.mainproject.server.image.entity.Image;
+import com.mainproject.server.search.dto.SearchDto;
+import com.mainproject.server.user.dto.UserDto;
+import com.mainproject.server.user.entity.User;
+import com.mainproject.server.user.repository.UserRepository;
+import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
+import org.openkoreantext.processor.tokenizer.KoreanTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import scala.collection.JavaConverters;
+
+@Service
+public class SearchService {
+
+
+    private FeedRepository feedRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public SearchService(UserRepository userRepository, FeedRepository feedRepository) {
+        this.userRepository = userRepository;
+        this.feedRepository = feedRepository;
+    }
+
+    public SearchDto searchByKeyword(String keyword) {
+        SearchDto searchDto = new SearchDto();
+
+        // 유저 정보 가져오기
+        List<User> users = userRepository.findByNicknameContaining(keyword);
+        List<UserDto.UserInfo> userInfos = users.stream()
+                .map(this::convertToUserInfo)
+                .collect(Collectors.toList());
+        searchDto.setUsers(userInfos);
+
+        // 피드 정보 가져오기
+        List<Feed> feeds = feedRepository.findFeedsByRelatedTags(keyword);
+        List<FeedDto.FeedInfo> feedInfos = feeds.stream()
+                .map(this::convertToFeedInfo)
+                .collect(Collectors.toList());
+        searchDto.setFeeds(feedInfos);
+
+        return searchDto;
+    }
+
+    private UserDto.UserInfo convertToUserInfo(User user) {
+        return UserDto.UserInfo.builder()
+                .userId(user.getUserId())
+                .nickname(user.getNickname())
+                .profileimg(user.getProfileimg() != null ? user.getProfileimg().getImageUrl() : null)
+                .build();
+    }
+
+    private FeedDto.FeedInfo convertToFeedInfo(Feed feed) {
+        return FeedDto.FeedInfo.builder()
+                .feedId(feed.getFeedId())
+                .content(feed.getContent())
+                .relatedTags(feed.getRelatedTags())
+                .images(feed.getImages().stream()
+                        .map(Image::getImageUrl)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
+
