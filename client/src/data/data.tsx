07927df4cdee96c6602905aaaa1f5ad5b9ef6ded
TaeYoungPar <tpark@oswego.edu>
2023-09-12 10:06:19 +0900

    style 스타일수정

--- client/src/data/data.tsx
+++ client/src/data/data.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const globalAxios = axios.create({
-  baseURL: "https://13.125.146.181:8080",
+  baseURL: "http://13.125.146.181:8080",
   timeout: 5000,
   headers: {
     "Content-Type": `application/json`,
-    "ngrok-skip-browser-warning": "69420",
   },
 });
 
