5bead36ae034cdcbf209e7178d3bfc722618e9d1
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2023-09-13 11:11:58 +0900

    Update FeedResponseDto.java

--- server/src/main/java/com/mainproject/server/feed/dto/FeedResponseDto.java
+++ server/src/main/java/com/mainproject/server/feed/dto/FeedResponseDto.java
@@ -1,7 +1,6 @@
 package com.mainproject.server.feed.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mainproject.server.image.entity.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
