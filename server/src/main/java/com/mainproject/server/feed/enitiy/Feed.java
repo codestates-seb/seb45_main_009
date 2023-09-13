d7a102c88d53feb064668358beef0fbe1cbd16bb
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2023-09-13 17:24:25 +0900

    Update Feed.java

--- server/src/main/java/com/mainproject/server/feed/enitiy/Feed.java
+++ server/src/main/java/com/mainproject/server/feed/enitiy/Feed.java
@@ -25,7 +25,7 @@ public class Feed {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long feedId;
 
-    @Colum
+    @Column
     private String nickname;
 
     @Column(columnDefinition = "TEXT")
