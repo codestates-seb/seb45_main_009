84e5979c8dcc5eab1d7f87b25ff71a0a516f2ba3
donghoonyeom <dua5410773@naver.com>
2023-09-13 14:04:11 +0900

    fix 피드 수정3

--- server/src/main/java/com/mainproject/server/feed/enitiy/Feed.java
+++ server/src/main/java/com/mainproject/server/feed/enitiy/Feed.java
@@ -25,7 +25,7 @@ public class Feed {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long feedId;
 
-    @Column(nullable = false)
+
     private String nickname;
 
     @Column(columnDefinition = "TEXT")
