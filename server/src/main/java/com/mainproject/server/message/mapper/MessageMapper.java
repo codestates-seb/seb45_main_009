5bdbaff89e16c6e92584df6294d7abb00ed9195e
donghoonyeom <dua5410773@naver.com>
2023-09-14 15:21:02 +0900

    feat Auditable

---
+++ server/src/main/java/com/mainproject/server/message/mapper/MessageMapper.java
@@ -0,0 +1,24 @@
+package com.mainproject.server.message.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import partypeople.server.message.dto.MessageDto;
+import partypeople.server.message.entity.Message;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MessageMapper {
+
+    @Mapping(source = "senderId", target = "sender.memberId")
+    @Mapping(source = "receiverId", target = "receiver.memberId")
+    @Mapping(source = "companionId", target = "companion.companionId")
+    Message messagePostToMessage(MessageDto.Post requestBody);
+
+    @Mapping(source = "sender.memberId", target = "sender.id")
+    @Mapping(source = "sender.nickname", target = "sender.nickname")
+    @Mapping(source = "companion.companionId", target = "companionId")
+    MessageDto.Response messageToMessageResponse(Message message);
+
+    List<MessageDto.Response> messagesToMessageResponses(List<Message> messages);
+}
