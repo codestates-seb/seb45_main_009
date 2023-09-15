d49d12bc3f5f3b41896de6f2a25e3ec07a8ca01c
donghoonyeom <102482877+donghoonyeom@users.noreply.github.com>
2023-09-15 09:56:58 +0900

    fix nickname 통합

--- server/src/main/java/com/mainproject/server/feed/dto/FeedResponseDto.java
+++ server/src/main/java/com/mainproject/server/feed/dto/FeedResponseDto.java
@@ -15,7 +15,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FeedResponseDto {
     private Long feedId;
-    private String userNickname;
+    private String nickname;
     private String location;
     private String profileImageUrl;
     private String content;
