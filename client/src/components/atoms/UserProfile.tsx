c5be2241fda83e5fd328d027fa8b1e392f1a2923
lse0522 <88942643+lse0522@users.noreply.github.com>
2023-09-17 00:55:54 +0900

    프로필페이지 수정

--- client/src/components/atoms/UserProfile.tsx
+++ client/src/components/atoms/UserProfile.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { AiFillHeart } from "react-icons/ai";
 import { BsFillBookmarkStarFill } from "react-icons/bs";
 import { MdAttachMoney } from "react-icons/md";
+import { BiSolidUser } from "react-icons/bi";
+
 
 
 interface ProfileIndProps {
@@ -13,6 +15,8 @@ interface ProfileIndProps {
   }
 
 function UserProfile({ userId, isMyFeed , myid }: ProfileIndProps){
+    const sport = ["헬스, 런닝"];
+
     const [isFollowing, setIsFollowing] = useState(false);
 
     const checkFollowState = async () => {
@@ -102,20 +106,12 @@ function UserProfile({ userId, isMyFeed , myid }: ProfileIndProps){
                         {userResponseType?.nickname}
                         {userResponseType?.roles.includes('STORE') && <BsFillBookmarkStarFill className="ml-2 mt-2 text-red-500" />}
                     </div>
-                    {(userResponseType?.height || userResponseType?.weight) && (
-                        <div className="flex">
-                            {userResponseType?.height && (
-                                <div className="mr-2.5 font-bold text-gray-400 text-sm">
-                                    {userResponseType?.height} cm
-                                </div>
-                            )}
-                            {userResponseType?.weight && (
-                                <div className="font-bold text-gray-400 text-sm ">
-                                    {userResponseType?.weight} kg
-                                </div>
-                            )}
-                        </div>
-                    )}
+                    <div className="opacity-60 text-[13px] max-mobile:text-[12px]">
+                    {userResponseType?.bio ? 
+                        userResponseType?.bio : 
+                        <span className="text-gray-400">__빈값__</span>
+                    }
+                    </div>
                 </div>
             </div>
             <div className="flex justify-center sm:justify-end mt-6  sm:mr-4">
@@ -138,13 +134,28 @@ function UserProfile({ userId, isMyFeed , myid }: ProfileIndProps){
         )}
             </div>
         </div>
-        <div className="mt-[30px] flex items-center justify-center sm:justify-start ">
-            {userResponseType?.bio && (
+        <div className="mt-[40px] flex items-center justify-center sm:justify-start ">
                 <>
+                {/* 개인이면 sport, 기업이면 위치 가져오기 */}
                     <AiFillHeart className="text-gray-400 mx-[10px] text-2xl"/>
-                    <div>{userResponseType?.bio}</div>
+                    <div>{sport}</div>
                 </>
-            )}
+        </div>
+        <div className="mt-2 flex items-center justify-center sm:justify-start ">
+            {
+                userResponseType?.roles.includes('USER') && (
+                    <>
+                        <BiSolidUser  className="text-gray-400 mx-[10px] text-2xl"/>
+                        <div>{userResponseType?.height ?
+                                userResponseType?.height :
+                                <span className="text-gray-400">__</span>} cm</div>
+                        <div className="ml-[14px]">
+                            {userResponseType?.weight ?
+                                userResponseType?.weight :
+                                <span className="text-gray-400">_</span>} kg</div>
+                    </>
+                )
+            }
         </div>
         <div className="mt-2 flex items-center justify-center sm:justify-start ">
             {userResponseType?.roles.includes('STORE') && (
@@ -154,6 +165,10 @@ function UserProfile({ userId, isMyFeed , myid }: ProfileIndProps){
                 </>
             )}
         </div>
+
+        {/* 선 추가 */}
+        <div className="my-[60px]  text-gray relative opacity-60 text-[13px] border-x flex justify-center">피드 리스트</div>
+
         <div className="my-[40px] flex flex-wrap justify-center sm:justify-start ">
             {userResponseType?.feedList.map((feed, feedIndex) => (
                 <div key={feedIndex} className="mb-[10px]">
