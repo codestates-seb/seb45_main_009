751d4e5374cee8a1b72ae2cc517e5ae863742558
donghoonyeom <dua5410773@naver.com>
2023-12-12 20:11:48 +0900

    refactor: Feed 클래스의 디렉토리명 변경
    - Feed 클래스의 디렉토리명 변경으로 인한 경로 변경

--- server/src/test/java/com/mainproject/server/feed/entity/FeedTest.java
+++ server/src/test/java/com/mainproject/server/feed/entity/FeedTest.java
@@ -1,4 +1,4 @@
-package com.mainproject.server.feed.enitiy;
+package com.mainproject.server.feed.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
