cceb689bcf22df378e1449077297d4175c9ca209
donghoonyeom <dua5410773@naver.com>
2023-12-12 20:29:05 +0900

    refactor(test): UserControllerTest 리팩토링
    UserControllerTest를 가독성과 구성성을 개선하기 위한 리팩토링을 수행하였습니다. 기존 테스트를 업데이트, JWT 인증과 관련된 일부 문제 해결이 필요
    
    - 명확성을 위해 테스트 메서드 이름 개선
    - 서로 다른 UserController 엔드포인트를 위한 별도의 테스트 케이스 분리
    - 테스트 데이터 초기화 프로세스 조정
    - JWT 인증과 관련된 일부 테스트 케이스에서 발생한 문제 해결 필요
    
    BREAKING CHANGE: 일부 JWT 인증에 의존하는 테스트 케이스가 현재 예상대로 작동하지 않습니다.

--- server/src/test/java/com/mainproject/server/user/controller/UserControllerTest.java
+++ server/src/test/java/com/mainproject/server/user/controller/UserControllerTest.java
@@ -1,18 +1,15 @@
 package com.mainproject.server.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.mainproject.server.image.service.ImageService;
+import com.mainproject.server.image.entity.Image;
 import com.mainproject.server.user.dto.AuthLoginDto;
 import com.mainproject.server.user.dto.UserDto;
 import com.mainproject.server.user.entity.User;
-import com.mainproject.server.user.repository.UserRepository;
 import com.mainproject.server.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,19 +20,14 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -46,243 +38,177 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private Gson gson;
-
-    @Autowired
     private ObjectMapper objectMapper;
-
-    @Autowired
-    UserRepository userRepository;
 
     @MockBean
     private UserService userService;
 
-    @Mock
-    private ImageService imageService;
-//
-    private MockMultipartFile userPostDto;
-    private MockMultipartFile profileImage;
-//    private UserDto.PostDto storePostDto;
-//    private UserDto.PatchDto patchDto;
-//    private AuthLoginDto authLoginDto;
-//    private User user;
+    private UserDto.PostDto userPostDto;
+    private UserDto.PostDto storePostDto;
+    private UserDto.PatchDto patchDto;
+    private AuthLoginDto authLoginDto;
+    private User user;
 
     @BeforeEach
     void setUp() {
         // 테스트에 사용할 데이터 초기화
-//        userPostDto = new UserDto.PostDto();
-//        userPostDto.setEmail("test@test.com");
-//        userPostDto.setPassword("test1111");
-//        userPostDto.setNickname("test");
-//        userPostDto.setBio("hello");
-//        userPostDto.setHeight(180);
-//        userPostDto.setWeight(80);
-//        userPostDto.setLocation("seoul");
-//        userPostDto.setSport("football");
-//        userPostDto.setProfileimg(new Image());
-
-//        storePostDto = new UserDto.PostDto();
-//        storePostDto.setEmail("store@test.com");
-//        storePostDto.setPassword("store1111");
-//        storePostDto.setNickname("store");
-//        storePostDto.setBio("hello");
-//        storePostDto.setHeight(180);
-//        storePostDto.setWeight(80);
-//        storePostDto.setLocation("seoul");
-//        storePostDto.setSport("football");
-//        storePostDto.setPrice("10000");
-
-//        authLoginDto = new AuthLoginDto();
-//        authLoginDto.setEmail("kakao@test.com");
-//        authLoginDto.setProfileimg("image");
-//
-//        patchDto = new UserDto.PatchDto();
-//
-//        user = new User();
-//        user.setEmail("user@test.com");
-//        user.setPassword("user1111");
-
-    }
-
-    @DisplayName("addUser: 유저 추가에 성공한다.")
-    @Test
-    void postUser() throws Exception {
-        //given
-        final String url = "/join/user";
-        UserDto.PostDto userPostDto = new UserDto.PostDto();
+        userPostDto = new UserDto.PostDto();
         userPostDto.setEmail("test@test.com");
         userPostDto.setPassword("test1111");
         userPostDto.setNickname("test");
-//        userPostDto.setBio("hello");
-//        userPostDto.setHeight(180);
-//        userPostDto.setWeight(80);
-//        userPostDto.setLocation("seoul");
-//        userPostDto.setSport("football");
-//        userPostDto.setProfileimg(new Image());
+        userPostDto.setBio("hello");
+        userPostDto.setHeight(180);
+        userPostDto.setWeight(80);
+        userPostDto.setLocation("seoul");
+        userPostDto.setSport("football");
+        userPostDto.setProfileimg(new Image());
 
-        // Create a MockMultipartFile for testing
-        byte[] fileContent = "Mock file content".getBytes();
-        MockMultipartFile profileImg = new MockMultipartFile("profileImg", "test.jpg", "image/jpeg", fileContent);
+        storePostDto = new UserDto.PostDto();
+        storePostDto.setEmail("store@test.com");
+        storePostDto.setPassword("store1111");
+        storePostDto.setNickname("store");
+        storePostDto.setBio("hello");
+        storePostDto.setLocation("seoul");
+        storePostDto.setSport("football");
+        storePostDto.setPrice("10000");
 
-        User user = new User();
-        user.setEmail("test@test.com");
-        user.setPassword("test1111");
-        user.setNickname("test");
+        authLoginDto = new AuthLoginDto();
+        authLoginDto.setEmail("kakao@test.com");
+        authLoginDto.setProfileimg("image");
 
-        given(userService.createUser(Mockito.any(User.class), Mockito.any(MockMultipartFile.class)))
-                .willReturn(user);
+        patchDto = new UserDto.PatchDto();
+        patchDto.setBio("안녕하세요");
+        patchDto.setHeight(170);
+        patchDto.setWeight(60);
 
-
-        String content = gson.toJson(userPostDto);
-        MockMultipartFile content1 = new MockMultipartFile("requestBody", content + ".json", "application/json", content.getBytes(StandardCharsets.UTF_8));
-
-        //when
-        ResultActions result =
-                mockMvc.perform(multipart(url, 1)
-                                .file(content1)
-                                .file(profileImg)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.MULTIPART_FORM_DATA)
-//                        .contentType(MediaType.MULTIPART_FORM_DATA)
-        );
-
-        //then
-        result
-                .andExpect(status().isCreated())
-                .andExpect(header().string("Location", is(startsWith("/user/"))))
-                .andDo(print());
+        user = new User();
+        user.setEmail("user@test.com");
+        user.setPassword("user1111");
     }
 
-
-//    @DisplayName("addUser: 유저 추가에 성공한다.")
-//    @Test
-//    void postUser() throws Exception {
-//        //given
-//        final String url = "/join/user";
-//        userPostDto = new UserDto.PostDto();
-//        userPostDto.setEmail("test@test.com");
-//        userPostDto.setPassword("test1111");
-//
-//        //when
-//        final ResultActions result = mockMvc.perform(post(url)
-//                .contentType(MediaType.MULTIPART_FORM_DATA)
-//                .requestAttr("requestBody", objectMapper.writeValueAsString(userPostDto))
-//        );
-//
-//        //then
-//        result
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.email").value(userPostDto.getEmail()));
-//    }
-
-
-
-    @DisplayName("addStore: 스토어 유저 추가에 성공한다.")
     @Test
-    void postStore() throws Exception {
-        // MockMultipartFile을 생성하여 profileimg 파트에 넣을 파일 데이터를 만듭니다.
-        MockMultipartFile file = new MockMultipartFile("profileimg", "test.jpg", "image/jpeg", "test file".getBytes());
+    @DisplayName("postUser: 유저 추가에 성공한다.")
+    void postUser() throws Exception {
+        // given
+        User createdUser = new User();
+        doReturn(createdUser).when(userService).createUser(any(), any());
 
-        // 테스트에 사용할 JSON 형식의 request body를 만듭니다.
-        String requestBodyJson = "{\"nickname\":null,\"email\":\"test@example.com\",\"password\":\"password123\",\"location\":null,\"profileimg\":null,\"price\":null,\"bio\":null,\"height\":null,\"weight\":null,\"sport\":null}";
+        // when
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/join/user")
+                        .file(new MockMultipartFile("requestBody", "", "application/json", asJsonString(userPostDto).getBytes()))
+                        .file(new MockMultipartFile("imageUrl", "test.jpeg", "image/png", "test image".getBytes()))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .accept(MediaType.APPLICATION_JSON));
 
-        // MockMvcRequestBuilders를 사용하여 POST 요청을 구성합니다.
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/join/store")
-                        .file(file)  // MultipartFile을 추가합니다.
-                        .content(requestBodyJson)  // JSON 형식의 request body를 추가합니다.
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.MULTIPART_FORM_DATA))
-                .andExpect(status().isCreated());  // 예상되는 HTTP 상태 코드를 지정합니다.
+        // then
+        result.andExpect(status().isCreated());
     }
 
-//    @DisplayName("addkakao: 카카오 유저 추가에 성공한다.")
-//    @Test
-//    void oAuth2LoginKakao() throws Exception {
-//        // given
-//        doReturn(user).when(userService).createUserOAuth2(any(), any());
-//
-//        // when
-//        ResultActions result = mockMvc.perform(post("/join/kakao")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(asJsonString(authLoginDto)));
-//
-//        // then
-//        result.andExpect(status().isOk());
-//    }
-//
-//    @DisplayName("getUserMypage: 유저 정보 조회에 성공한다.")
-//    @Test
-//    @WithMockUser   // Mock 사용자로 테스트 진행
-//    void getUserMypage() throws Exception {
-//        // given
-//        when(userService.findUser(anyLong())).thenReturn(user);
-//
-//        // when
-//        ResultActions result = mockMvc.perform(get("/mypage")
-//                .contentType(MediaType.APPLICATION_JSON));
-//
-//        // then
-//        result.andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").exists())
-//                .andExpect(jsonPath("$.data.username").value(user.getUserId()));
-//    }
-//
-//
-//
-//
-//    @DisplayName("getUsers: 유저 전체 조회에 성공한다.")
-//    @Test
-//    @WithMockUser
-//    void getUsers() throws Exception {
-//        // given
-//        List<User> userList = List.of(user, new User(), new User());
-//        doReturn(userList).when(userService).findAllUsers();
-//
-//        // when
-//        ResultActions result = mockMvc.perform(get("/users")
-//                .contentType(MediaType.APPLICATION_JSON));
-//
-//        // then
-//        result.andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isArray())
-//                .andExpect(jsonPath("$.length()").value(userList.size()));
-//    }
-//
-//    @DisplayName("patchUser: 유저 정보 수정에 성공한다.")
-//    @Test
-//    @WithMockUser
-//    void patchUser() throws Exception {
-//        // given
-//        doReturn(Optional.of(user)).when(userService).findUser(anyLong());
-//        doReturn(user).when(userService).updateUser(anyLong(), any(User.class), any());
-//
-//        // when
-//        ResultActions result = mockMvc.perform(patch("/mypage/update")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(asJsonString(patchDto)));
-//
-//        // then
-//        result.andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").exists())
-//                .andExpect(jsonPath("$.data.email").value(user.getEmail()));
-//    }
-//
-//    @DisplayName("deleteUser: 유저 삭제에 성공한다.")
-//    @Test
-//    @WithMockUser
-//    void deleteUser() throws Exception {
-//        // given
-//        doReturn(Optional.of(user)).when(userService).findUser(anyLong());
-//
-//        // when
-//        ResultActions result = mockMvc.perform(delete("/mypage/delete")
-//                .contentType(MediaType.APPLICATION_JSON));
-//
-//        // then
-//        result.andExpect(status().isNoContent());
-//    }
-//
-//    private String asJsonString(Object object) throws Exception {
-//        return objectMapper.writeValueAsString(object);
-//    }
+    @Test
+    @DisplayName("postStore: 스토어 유저 추가에 성공한다.")
+    void postStore() throws Exception {
+        // given
+        User createdUser = new User();
+        doReturn(createdUser).when(userService).createUser(any(), any());
+
+        // when
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/join/store")
+                        .file(new MockMultipartFile("requestBody", "", "application/json", asJsonString(storePostDto).getBytes()))
+                        .file(new MockMultipartFile("imageUrl", "test.jpeg", "image/png", "store test image".getBytes()))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("addkakao: 카카오 유저 추가에 성공한다.")
+    void oAuth2LoginKakao() throws Exception {
+        // given
+        doReturn(user).when(userService).createUserOAuth2(any(), any());
+
+        // when
+        ResultActions result = mockMvc.perform(post("/join/kakao")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(authLoginDto)));
+
+        // then
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("getUserMypage: 유저 정보 조회에 성공한다.")
+    @WithMockUser
+    void getUserMypage() throws Exception {
+        // given
+        doReturn(Optional.of(user)).when(userService).findUser(anyLong());
+
+        // when
+        ResultActions result = mockMvc.perform(get("/mypage")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.username").value(user.getUserId()));
+    }
+
+    @Test
+    @DisplayName("getUsers: 유저 전체 조회에 성공한다.")
+    @WithMockUser
+    void getUsers() throws Exception {
+        // given
+        List<User> userList = List.of(user, new User(), new User());
+        doReturn(userList).when(userService).findAllUsers();
+
+        // when
+        ResultActions result = mockMvc.perform(get("/users")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(userList.size()));
+    }
+
+    @DisplayName("patchUser: 유저 정보 수정에 성공한다.")
+    @Test
+    @WithMockUser
+    void patchUser() throws Exception {
+        // given
+        doReturn(user).when(userService).findUser(anyLong());
+        doReturn(user).when(userService).updateUser(anyLong(), any(User.class), any());
+
+        // when
+        ResultActions result = mockMvc.perform(patch("/mypage/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(patchDto)));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.bio").value(user.getBio()));
+    }
+
+    @Test
+    @DisplayName("deleteUser: 유저 삭제에 성공한다.")
+    @WithMockUser
+    void deleteUser() throws Exception {
+        // given
+        doReturn(Optional.of(user)).when(userService).findUser(anyLong());
+
+        // when
+        ResultActions result = mockMvc.perform(delete("/mypage/delete")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isNoContent());
+    }
+
+    private String asJsonString(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
 }
